ad5f5980a27d4107962da9c55fac0640679b85c6
Robin Schmidt <schmidt.rbn@gmail.com>
2022-10-03 16:21:38 +0200

    ADD: Detail componant, set up routes

---
+++ src/Components/PokemonDetail.jsx
@@ -0,0 +1,13 @@
+import { useParams } from 'react-router-dom';
+import useApi from './useApi';
+import './PokemonDetail.css';
+
+function PokemonDetail() {
+  return (
+    <div>
+      <h2>PokemonDetail</h2>
+    </div>
+  )
+}
+
+export default PokemonDetail
