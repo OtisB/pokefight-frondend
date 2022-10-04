fd542fcee7e16ee63ed06bf962145bc604307ca6
Roland Kirchhoff <rolandkirchhoff@web.de>
2022-10-04 17:15:11 +0200

    Grid corerctions

--- src/Components/PokemonContainer.jsx
+++ src/Components/PokemonContainer.jsx
@@ -6,10 +6,6 @@ import React from 'react'
 
 function PokemonContainer({ allPokemons }) {
 
-  const half = Math.ceil(allPokemons.length / 2);
-
-  const upperRow = allPokemons.slice(0, half)
-  const bottomRow = allPokemons.slice(half)
 
   return (
     <div className="pokemon-list-frame">
