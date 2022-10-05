389bd34e8a994121ccf250ce7570e17baefedef6
Robin Schmidt <schmidt.rbn@gmail.com>
2022-10-05 22:53:27 +0200

    MODIFIED: Added Placeholder for animation

--- src/Components/FightingArea.jsx
+++ src/Components/FightingArea.jsx
@@ -6,21 +6,34 @@ import './FightingArea.css'
 
 function FightingArea() {
 
-  const [fightStartet, setFightStartet] = useState(false);
+  const [wildPkmApeares, setWildPkmApeares] = useState(false);
+  const [showPlaceholder, setShowPlaceholder] = useState(false);
+  const [showResults, setShowResults] = useState(false);
 
   const navigate = useNavigate();
   const { id } = useParams();
   const pokemons = useApi();
 
   const originalPkm = pokemons.slice(0, 150);
-
   const currPkm = originalPkm.find(pkm => pkm.id == id);
   const randomId = Math.floor(Math.random() * originalPkm.length);
   const rdmPkm = originalPkm.find(pkm => pkm.id === randomId);
 
-  const handleFightStartet = ((event) => {
+  useEffect(() => {
+    console.log('');
+  }, [showPlaceholder]);
+
+  const startDelay = () => {
+    setTimeout(() => {
+      console.log('Delaying');
+      setShowPlaceholder(setShowPlaceholder(true));
+    }, 5000);
+  };
+
+  const handleWildPkmApeares = ((event) => {
     event.preventDefault();
-    setFightStartet(setFightStartet(true));
+    startDelay();
+    setWildPkmApeares(setWildPkmApeares(true));  //But why? setWildPkmApeares(setWildPkmApeares(true)); and not setWildPkmApeares(true);
   });
 
   return (
@@ -28,9 +41,10 @@ function FightingArea() {
       <button onClick={() => navigate(`/`)}>Choose an other Pokemon</button>
       <div className="fighting-area-container">
         {currPkm ? (<PokemonDetail pokemon={currPkm} />) : <h2>Couldn't find your Pokemon!</h2>}
-        {(fightStartet && rdmPkm) ?? <PokemonDetail pokemon={rdmPkm} />}
+        {showPlaceholder ?? <h2>Animation Placeholder</h2>}
+        {wildPkmApeares ?? <PokemonDetail pokemon={rdmPkm} />}
       </div>
-      <button onClick={handleFightStartet}>Start fight</button>
+      <button disabled={wildPkmApeares === true} onClick={handleWildPkmApeares}>Start fight</button>
     </section>
   )
 }
