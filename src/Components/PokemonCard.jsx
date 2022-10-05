8117fabfa2344dd10b7aa2d3912731cce4f1c65b
Roland Kirchhoff <rolandkirchhoff@web.de>
2022-10-05 17:20:26 +0200

    scrollbutton Korrektur

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -7,7 +7,7 @@ function PokemonCard({ pokemon }) {
     <Link to={`/pokemon/${pokemon.id}`}>
       <section className="poke-card" key={pokemon.id}>
         <div className="card-image-container">
-          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.png`} alt={pokemon.name.english} />
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} alt={pokemon.name.english} />
         </div>
         <div className="card-content">
           <h2>{pokemon.name.english}</h2>
