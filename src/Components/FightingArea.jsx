caea24f036cd6c8b37d54a2f70e5369d26c380e2
Robin Schmidt <schmidt.rbn@gmail.com>
2022-10-05 16:59:42 +0200

    MODIFIED: fixed typo, renamed state

--- src/Components/FightingArea.jsx
+++ src/Components/FightingArea.jsx
@@ -6,21 +6,21 @@ import './FightingArea.css'
 
 function FightingArea() {
 
-  const [isFighting, setIsFighting] = useState(false);
+  const [fightStartet, setFightStartet] = useState(false);
 
   const navigate = useNavigate();
   const { id } = useParams();
   const pokemons = useApi();
 
-  const orriginalPkm = pokemons.slice(0, 150);
+  const originalPkm = pokemons.slice(0, 150);
 
-  const currPkm = orriginalPkm.find(pkm => pkm.id == id);
-  const randomId = Math.floor(Math.random() * orriginalPkm.length);
-  const rdmPkm = orriginalPkm.find(pkm => pkm.id === randomId);
+  const currPkm = originalPkm.find(pkm => pkm.id == id);
+  const randomId = Math.floor(Math.random() * originalPkm.length);
+  const rdmPkm = originalPkm.find(pkm => pkm.id === randomId);
 
-  const handleIsFighting = ((event) => {
+  const handleFightStartet = ((event) => {
     event.preventDefault();
-    setIsFighting(setIsFighting(true));
+    setFightStartet(setFightStartet(true));
   });
 
   return (
@@ -28,9 +28,9 @@ function FightingArea() {
       <button onClick={() => navigate(`/`)}>Choose an other Pokemon</button>
       <div className="fighting-area-container">
         {currPkm ? (<PokemonDetail pokemon={currPkm} />) : <h2>Couldn't find your Pokemon!</h2>}
-        {(isFighting && rdmPkm) ?? <PokemonDetail pokemon={rdmPkm} />}
+        {(fightStartet && rdmPkm) ?? <PokemonDetail pokemon={rdmPkm} />}
       </div>
-      <button onClick={handleIsFighting}>Start fight</button>
+      <button onClick={handleFightStartet}>Start fight</button>
     </section>
   )
 }
