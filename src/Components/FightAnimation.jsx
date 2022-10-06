fdca218482143b6705a5cf75cd6b45aefa4a0379
Roland Kirchhoff <rolandkirchhoff@web.de>
2022-10-06 14:50:36 +0200

    Animation

---
+++ src/Components/FightAnimation.jsx
@@ -0,0 +1,42 @@
+import './FightingArea.css'
+
+function FightAnimation() {
+
+
+    function wind() {
+        const direction = Math.random() * 100;
+    
+        document.getElementById('staubwolke').style.marginLeft = parseInt(window.getComputedStyle(document.getElementById('staubwolke')).marginLeft) + parseInt(direction) + "px";
+    
+        const blitz_offset = Math.random() * 150;
+        document.getElementById('blitz').style.marginLeft = parseInt(direction) + "px";
+        document.getElementById('blitz').style.marginTop = parseInt(blitz_offset) + "px";
+    
+        console.log(direction, blitz_offset, window.getComputedStyle(document.getElementById('staubwolke')).marginLeft);
+        
+        if (document.getElementById('animation').className.indexOf('blitzed') === -1) {
+            document.getElementById('animation').className = 'animated blitzed';
+        } else {
+            document.getElementById('animation').className = 'animated';
+        }
+    }
+
+    const animInterval = window.setInterval(wind, 800);
+
+    return (
+
+        <div id="animation">
+    
+            <div id="box"></div> 
+            <div id="stern"></div> 
+            <div id="stern2"></div> 
+            <div id="stern3"></div> 
+            <div id="staubwolke"></div>
+            <div id="staubwolke2"></div>
+            <div id="blitz"></div>
+
+        </div>
+    )
+}
+
+export default FightAnimation
